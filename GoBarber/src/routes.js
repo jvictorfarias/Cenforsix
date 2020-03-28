f17fbf6be2f7a23a29d4e3b7b0587108d344da03
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-03-28 09:12:01 -0300

    Aula04: Primeira parte

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
+
+routes.get('/users', async (req, res) => {
+  const user = await User.create({
+    name: 'Joao Victor',
+    email: 'victorfarias.new@gmail.com',
+    password_hash: '12345',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
--- GoBarber/src/server.js
+++ GoBarber/src/server.js
@@ -1,3 +1,6 @@
+import ora from 'ora';
 import app from './app';
+
+ora('Server running...').start();
 
 app.listen(3333);
