27906b5bbf46fe2c518269ddc542710de75f80ac
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-04-18 11:57:39 -0300

    aula06 finalizada

---
+++ GoBarber/src/app/controllers/AppointmentController.js
@@ -0,0 +1,95 @@
+import * as Yup from 'yup';
+import { parseISO, startOfHour, isBefore } from 'date-fns';
+
+import User from '../models/User';
+import File from '../models/File';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.status(200).json(appointments);
+  }
+
+  async store(req, res) {
+    // data, user_id, provider_id
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Provider or date invalid' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res.status(400).json({ error: 'Provider not found' });
+    }
+
+    /**
+     * ! parseISO: pega a data no formato string e transforma em objeto Date
+     * ! A função isBefore vai verificar se a data já passou
+     */
+
+    const parsedDate = startOfHour(parseISO(date));
+
+    if (isBefore(parsedDate, new Date())) {
+      return res.status(401).json({ error: 'Past dates are not allowed.' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        date: parsedDate,
+        canceled_at: null,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(401)
+        .json({ error: "Can't make appointment on this date" });
+    }
+
+    if (req.userId === provider_id) {
+      return res
+        .status(401)
+        .json({ error: "Can't make appointments with himself" });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date: parsedDate,
+    });
+
+    return res.status(200).json(appointment);
+  }
+}
+
+export default new AppointmentController();
--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -18,6 +19,10 @@ routes.put('/users', authMiddleware, UserController.update);
 
 // Rotas de providers
 routes.get('/providers', authMiddleware, ProviderController.index);
+
+// Rotas de agendamentos
+routes.post('/appointments', authMiddleware, AppointmentController.store);
+routes.get('/appointments', authMiddleware, AppointmentController.index);
 
 // Rota de sessão
 routes.post('/session', SessionController.store);
