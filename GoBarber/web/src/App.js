d3f3d38c4d343a572aab3167349881adf49f8cea
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-04-27 21:42:12 -0300

    aula07: iniciando o frontend

---
+++ GoBarber/web/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <ul>
+        <li>teste1</li>
+        <li>teste2</li>
+        <li>teste3</li>
+      </ul>
+    </>
+  );
+};
+
+export default App;
+
+// React
+// ReactJS - SPA's e React Native
---
+++ GoBarber/web/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/global.css';
+import App from './App';
+
+// JSX
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
