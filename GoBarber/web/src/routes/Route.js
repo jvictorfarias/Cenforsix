ab77c24fb5fde471bc66c4af9d21c1bed5930ce8
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-05-02 09:53:26 -0300

    aula08: primeira parte

---
+++ GoBarber/web/src/routes/Route.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route as RouteDOM, Redirect } from 'react-router-dom';
+
+const Route = ({ component: Component, isPrivate, ...rest }) => {
+  const signed = true;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return <RouteDOM {...rest} component={Component} />;
+};
+
+export default Route;
+
+Route.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+Route.defaultProps = {
+  isPrivate: false,
+};
