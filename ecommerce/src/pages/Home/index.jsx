0c8d5044521309678af6191d7824d7616cc23cab
jvictorfarias <victorfarias.new@gmail.com>
2020-05-30 09:49:14 -0300

    :boom: intervalo da aula

--- ecommerce/src/pages/Home/index.jsx
+++ ecommerce/src/pages/Home/index.jsx
@@ -1,7 +1,119 @@
 import React from 'react';
 
+import { FiShoppingCart } from 'react-icons/fi';
+
+import { ProductList, Product } from './styles';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <Product>
+        <img
+          src="https://http2.mlstatic.com/faqueiro-ipanema-aco-inox-30-pcs-23398088-tramontina-D_NQ_NP_819687-MLB32853847000_112019-O.webp"
+          alt="faqueiro"
+        />
+        <strong>Faqueiro X-9000</strong>
+        <span>R$120,00</span>
+
+        <button type="button">
+          <div>
+            <FiShoppingCart size={18} /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </Product>
+      <Product>
+        <img
+          src="https://http2.mlstatic.com/faqueiro-ipanema-aco-inox-30-pcs-23398088-tramontina-D_NQ_NP_819687-MLB32853847000_112019-O.webp"
+          alt="faqueiro"
+        />
+        <strong>Faqueiro X-9000</strong>
+        <span>R$120,00</span>
+
+        <button type="button">
+          <div>
+            <FiShoppingCart size={18} />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </Product>
+      <Product>
+        <img
+          src="https://http2.mlstatic.com/faqueiro-ipanema-aco-inox-30-pcs-23398088-tramontina-D_NQ_NP_819687-MLB32853847000_112019-O.webp"
+          alt="faqueiro"
+        />
+        <strong>Faqueiro X-9000</strong>
+        <span>R$120,00</span>
+
+        <button type="button">
+          <div>
+            <FiShoppingCart size={18} />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </Product>
+      <Product>
+        <img
+          src="https://http2.mlstatic.com/faqueiro-ipanema-aco-inox-30-pcs-23398088-tramontina-D_NQ_NP_819687-MLB32853847000_112019-O.webp"
+          alt="faqueiro"
+        />
+        <strong>Faqueiro X-9000</strong>
+        <span>R$120,00</span>
+
+        <button type="button">
+          <div>
+            <FiShoppingCart size={18} />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </Product>
+      <Product>
+        <img
+          src="https://http2.mlstatic.com/faqueiro-ipanema-aco-inox-30-pcs-23398088-tramontina-D_NQ_NP_819687-MLB32853847000_112019-O.webp"
+          alt="faqueiro"
+        />
+        <strong>Faqueiro X-9000</strong>
+        <span>R$120,00</span>
+
+        <button type="button">
+          <div>
+            <FiShoppingCart size={18} />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </Product>
+      <Product>
+        <img
+          src="https://http2.mlstatic.com/faqueiro-ipanema-aco-inox-30-pcs-23398088-tramontina-D_NQ_NP_819687-MLB32853847000_112019-O.webp"
+          alt="faqueiro"
+        />
+        <strong>Faqueiro X-9000</strong>
+        <span>R$120,00</span>
+
+        <button type="button">
+          <div>
+            <FiShoppingCart size={18} />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </Product>
+      <Product>
+        <img
+          src="https://http2.mlstatic.com/faqueiro-ipanema-aco-inox-30-pcs-23398088-tramontina-D_NQ_NP_819687-MLB32853847000_112019-O.webp"
+          alt="faqueiro"
+        />
+        <strong>Faqueiro X-9000</strong>
+        <span>R$120,00</span>
+
+        <button type="button">
+          <div>
+            <FiShoppingCart size={18} />
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </Product>
+    </ProductList>
+  );
 };
 
 export default Home;
--- ecommerce/src/pages/Home/styles.jsx
+++ ecommerce/src/pages/Home/styles.jsx
@@ -1,3 +1,67 @@
 import styled from 'styled-components';
+import { lighten } from 'polished';
 
-export const Container = styled.div``;
+export const ProductList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+`;
+
+export const Product = styled.li`
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+
+  img {
+    align-self: center;
+    max-width: 240px;
+  }
+
+  > strong {
+    font-size: 16px;
+    line-height: 20px;
+    color: #333;
+    margin-top: 8px;
+  }
+
+  > span {
+    font-size: 22px;
+    font-weight: 500;
+    margin: 5px 0 20px;
+  }
+
+  button {
+    background: #322c38;
+    color: #f8f8f2;
+    border: 0;
+    border-radius: 4px;
+    margin-top: auto;
+    overflow: hidden;
+
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      background: ${lighten(0.08, '#322c38')};
+    }
+
+    div {
+      background: #19181f;
+      display: flex;
+      align-items: center;
+      padding: 12px;
+
+      svg {
+        margin-right: 5px;
+      }
+    }
+
+    span {
+      flex: 1;
+      text-align: center;
+      font-weight: 500;
+    }
+  }
+`;
